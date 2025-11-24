f66f1d85c9da65be1a35cef8f21cbe2936cf4de9
Kitsai <lucas.rocha.3801@gmail.com>
2025-11-24 13:42:46 -0300

    user model creation

---
+++ CAMAAR/db/migrate/20251124163014_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[8.1]
+  def change
+    create_table :users, id: false  do |t|
+      t.integer :id, null: false, primary_key: true
+      t.string :email, null: false
+      t.string :name
+      t.string :password_digest
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
