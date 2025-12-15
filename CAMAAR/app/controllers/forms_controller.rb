e390a85486f7a5213c0d254ffb4db2f435a6e20f
Marcolino5 <102527399+Marcolino5@users.noreply.github.com>
2025-12-15 00:50:55 -0300

    altered forms_controller for reduced flog

--- CAMAAR/app/controllers/forms_controller.rb
+++ CAMAAR/app/controllers/forms_controller.rb
@@ -12,44 +12,19 @@ class FormsController < ApplicationController
   end
 
   def create
-    template_id = params[:template_id]
-    course_ids  = params[:course_ids]
-
-    if template_id.blank? && !course_ids.blank?
-      redirect_to forms_path, alert: "É necessário selecionar um template"
-      return
-    end
-
-    if course_ids.blank? && !template_id.blank?
-      redirect_to forms_path, alert: "É necessário selecionar pelo menos uma turma"
-      return
-    end
-
-    template = Template.find(params[:template_id])
-
-    ActiveRecord::Base.transaction do
-      course_ids.each do |course_id|
-        course = Course.find(course_id)
-
-        form = Form.create!(
-          admin: current_user.admin,
-          course: course,
-          question_set_id: template.question_set_id
-        )
-
-        # Create FormRequests for students + teacher
-        recipients = course.students.to_a
-        recipients << course.teacher if course.teacher.present?
-
-        recipients.each do |user|
-          FormRequest.find_or_create_by!(user: user, form: form)
-        end
-      end
-    end
+    CreateFormService.call(
+      admin: current_user.admin,
+      template_id: params[:template_id],
+      course_ids: params[:course_ids]
+    )
 
     redirect_to forms_path, notice: "Formulários criados com sucesso!"
-  rescue ActiveRecord::RecordNotFound
-    redirect_to forms_path, alert: "Template e curso inválidos"
+
+  rescue CreateFormService::MissingTemplate
+    redirect_to forms_path, alert: "É necessário selecionar um template"
+
+  rescue CreateFormService::MissingCourses
+    redirect_to forms_path, alert: "É necessário selecionar pelo menos uma turma"
   end
 
   def results
